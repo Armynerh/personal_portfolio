bafe402f54049a096efa5f5b01eec3dec74c1fda
Ngala Mac <macngala4@gmail.com>
2023-06-26 10:15:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const toggler = document.querySelector('.nav-toggle');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const toggleIcon = document.querySelector('.toggle-icon');
 
-toggler.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('active');
-});
 
 toggleIcon.addEventListener('click', () => {
   hamburgerMenu.classList.remove('active');
