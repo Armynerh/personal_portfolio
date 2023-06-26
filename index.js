16ec0689a564c469a55c89751291f105f4bd98a2
Armynerh <aimenah@outlook.com>
2023-06-26 09:42:46 +0100

    change linters configurations

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const toggler = document.querySelector('.nav-toggle');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const toggleIcon = document.querySelector('.toggle-icon');
 
-toggler.addEventListener('click', () =>{
-    hamburgerMenu.classList.toggle('active');
+toggler.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active');
 });
 
-toggleIcon.addEventListener('click', () =>{
-    hamburgerMenu.classList.remove('active');
+toggleIcon.addEventListener('click', () => {
+  hamburgerMenu.classList.remove('active');
 });
-document.querySelectorAll('.nav-item').forEach(n => n.addEventListener("click", () =>{
-    hamburgerMenu.classList.remove('active');
-}))
+document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click', () => {
+  hamburgerMenu.classList.remove('active');
+}));
